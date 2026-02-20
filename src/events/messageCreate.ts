d00cea11104b694af53ed8aa131b387589c2bbad
Alex Tavares <alexdasilvamassanatavares@gmail.com>
2026-02-20 14:15:59 +0100

    .

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -2,10 +2,15 @@ import { Message } from "discord.js";
 import type { Command } from "../types/command.ts";
 import { CountingGame } from "../handlers/CountingGame.ts";
 
+const countingGame = new CountingGame();
+
 export default (
   message: Message,
   commands: Map<string, Command>
 ) => {
+
+  await countingGame.handleMessage(message);
+
   if (!message.content.startsWith("!")) return;
 
   const args = message.content.slice(1).split(" ");
